8edcbde8723bb122e6b927505a8fb39c52e0463b
Peter Go <189393454+peter89530@users.noreply.github.com>
2025-10-03 04:57:30 +0900

    change interface and readme; add interface

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,51 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client"
+
+import { useState } from "react";
+import { v4 as uuid } from "uuid";
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    let [output, setOutput] = useState<string>("");
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+
+    async function createRequest(requestingSentence: string, apiKey: string) {
+        setOutput("Making a request through the Yahoo!デベロッパーネットワーク credentials provided...");
+
+        const jsonRpcContents = {
+            "id": uuid(),
+            "jsonrpc": "2.0",
+            "method": "jlp.furiganaservice.furigana",
+            "params": {
+                "q": requestingSentence,
+                "grade": 1
+            }
+        }
+
+        const headers = new Headers();
+        headers.append("Content-Length", `${Buffer.byteLength(JSON.stringify(jsonRpcContents), "utf8")}`);
+        headers.append("User-Agent", `Yahoo AppID: ${apiKey}`);
+
+        const results = await fetch("https://jlp.yahooapis.jp/FuriganaService/V2/furigana", {
+            mode: "no-cors",
+            method: "POST",
+            headers: headers,
+            body: JSON.stringify(jsonRpcContents),
+        });
+
+        console.info(results.json());
+    }
+
+    return (
+        <main>
+            <h1>rubifuri (ルビ振り) Interface</h1>
+            <p>Note: A Yahoo!デベロッパーネットワーク Account and Client ID are required to access the API. All usage of the API is subject to the Yahoo!デベロッパーネットワーク利用規約. This project simply aims to create an interface for the API.</p>
+            <input id="apiKey" type="password" placeholder="App ID"></input>
+            <input id="in" placeholder="Kanji Input"></input>
+            <button onClick={(event) => {
+                const input = (document.getElementById("in") as HTMLInputElement).value;
+                const key = (document.getElementById("apiKey") as HTMLInputElement).value;
+                createRequest(input, key);
+            }}>Request Furigana</button>
+            <p>{output}</p>
+        </main>
+    );
 }
