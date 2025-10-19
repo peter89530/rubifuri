e4e8e67879b89c6d3b9fbb4f670d9a5e50786ea1
peter89530 <189393454+peter89530@users.noreply.github.com>
2025-10-19 15:43:34 +0900

    update to view body through devtools; project roadblock

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 "use client"
 
 import { useState } from "react";
-import { v4 as uuid } from "uuid";
 
 export default function Home() {
     let [output, setOutput] = useState<string>("");
@@ -9,40 +8,78 @@ export default function Home() {
 
     async function createRequest(requestingSentence: string, apiKey: string) {
         setOutput("Making a request through the Yahoo!デベロッパーネットワーク credentials provided...");
-
-        const jsonRpcContents = {
-            "id": uuid(),
+        
+        const params = new URLSearchParams({
+            "appid": apiKey
+        }).toString();
+        const requestEndpoint = "https://jlp.yahooapis.jp/FuriganaService/V2/furigana?" + params;
+        
+        const query = {
+            "id": 812,
             "jsonrpc": "2.0",
             "method": "jlp.furiganaservice.furigana",
             "params": {
                 "q": requestingSentence,
-                "grade": 1
+                "grade": 1,
             }
         }
-
+        
         const headers = new Headers();
-        headers.append("Content-Length", `${Buffer.byteLength(JSON.stringify(jsonRpcContents), "utf8")}`);
-        headers.append("User-Agent", `Yahoo AppID: ${apiKey}`);
-
-        const results = await fetch("https://jlp.yahooapis.jp/FuriganaService/V2/furigana", {
+        headers.set("Content-Type", "application/json");
+        
+        const response = await fetch(requestEndpoint, {
             mode: "no-cors",
             method: "POST",
-            headers: headers,
-            body: JSON.stringify(jsonRpcContents),
-        });
+            body: JSON.stringify(query),
+            headers: headers
+        })
 
-        console.info(results.json());
+        // This will never work because of browser security issues. Yahoo is returning a perfectly fine response, but JS can never access a cross-origin response body. You can use devtools to view it tho
+        console.log(response);
+        setTimeout(() => {
+            console.log(response);
+        },1000);
+
+        // .then((results) => {
+        //     if (results.status === 401) {
+        //         setOutput("There was an Authentication failure. The error code is 401.");
+        //         return;
+        //     }
+    
+            
+        //     console.log(results.headers);
+        //     console.log(results.arrayBuffer());
+        //     console.log(results.text());
+        //     console.log(results.json());
+        //     console.log(results.bytes());
+        // }).then((readOutput) => {
+        //     if (!readOutput.done) {
+        //         setOutput("There was a response processing error.");
+        //         return;
+        //     }
+            
+        //     setOutput("Done!");
+        //     console.info(readOutput.value);
+        // }).catch((error) => {
+        //     if (error)
+        //     setOutput("The Yahoo!デベロッパーネットワーク denied your request or responded with a malformed response.");
+        //     console.error(error);
+        // })
     }
 
     return (
         <main>
             <h1>rubifuri (ルビ振り) Interface</h1>
-            <p>Note: A Yahoo!デベロッパーネットワーク Account and Client ID are required to access the API. All usage of the API is subject to the Yahoo!デベロッパーネットワーク利用規約. This project simply aims to create an interface for the API.</p>
+            <p>Note: This open-source project simply aims to create an client side interface for the API.</p>
+            
+            <span style={{textDecoration: "unset", margin: "15px 15px 15px 15px"}}><a href="https://developer.yahoo.co.jp/sitemap/">Webサービス by Yahoo! JAPAN</a></span>
+
             <input id="apiKey" type="password" placeholder="App ID"></input>
             <input id="in" placeholder="Kanji Input"></input>
             <button onClick={(event) => {
                 const input = (document.getElementById("in") as HTMLInputElement).value;
                 const key = (document.getElementById("apiKey") as HTMLInputElement).value;
+                console.log(input, key);
                 createRequest(input, key);
             }}>Request Furigana</button>
             <p>{output}</p>
